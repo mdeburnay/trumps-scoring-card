7854d0c6a9718024e8ccb0a6e28da2c6fa875b74
Max De Burnay-Bastos <de_burnay@hotmail.co.uk>
2024-03-07 17:02:27 +1100

    made screen options

--- App.tsx
+++ App.tsx
@@ -4,6 +4,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { View } from "react-native";
 
+// Utils
+import { screenOptions } from "./utils/screenOptions";
+
 type RootStackParamList = {
   Home: undefined;
 };
@@ -15,7 +18,7 @@ const RootStack = createNativeStackNavigator<RootStackParamList>();
 function App() {
   return (
     <NavigationContainer>
-      <RootStack.Navigator>
+      <RootStack.Navigator screenOptions={screenOptions}>
         <RootStack.Screen name="Home" component={Home} />
       </RootStack.Navigator>
     </NavigationContainer>
