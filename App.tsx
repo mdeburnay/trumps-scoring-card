f9da4cc8a02e4df208488a62ee1d6837c69efc11
Max De Burnay-Bastos <de_burnay@hotmail.co.uk>
2024-03-07 17:08:35 +1100

    basic styles

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 // Packages
 import * as React from "react";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { View } from "react-native";
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 // Utils
 import { screenOptions } from "./utils/screenOptions";
@@ -11,7 +13,13 @@ type RootStackParamList = {
   Home: undefined;
 };
 
-const Home = () => <View />;
+const Home = () => (
+  <SafeAreaView style={styles.container}>
+    <View>
+      <Text style={styles.text}>Trumps</Text>
+    </View>
+  </SafeAreaView>
+);
 
 const RootStack = createNativeStackNavigator<RootStackParamList>();
 
@@ -24,5 +32,18 @@ function App() {
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+    alignItems: "center",
+    backgroundColor: "#6495ED",
+  },
+  text: {
+    fontSize: 40,
+    color: "#FFFFFF",
+  },
+});
 
 export default App;
